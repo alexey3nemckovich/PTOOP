fb28ec366ba3abbff33ed8af901df8b951f4507c
Alex Nemkovich <alexey3nemckovich@gmail.com>
2019-03-27 20:58:56 +0300

    Adaptered plugin.

---
+++ SomeFriendPlugin/FriendPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace SomeFriendPlugin
+{
+    public class FriendPlugin
+    {
+        public void DoSave(string message)
+        {
+            MessageBox.Show(
+                "Some friend plugin on save:" + message,
+                "",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.None);
+        }
+
+        public void DoLoad(string message)
+        {
+            MessageBox.Show(
+                "Some friend plugin on load: " + message,
+                "",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.None);
+        }
+    }
+}
---
+++ SomeFriendPluginAdapter/SomeFriendPluginAdapter.cs
@@ -0,0 +1,65 @@
+﻿using SomeFriendPlugin;
+using GraphicsEditor.Engine;
+using GraphicsEditor.Serialization;
+
+namespace SomeFriendPluginAdapter
+{
+    public class SomeFriendPluginAdapter : Plugin
+    {
+        FriendPlugin friendPlugin = new FriendPlugin();
+
+        private static string friendPluginMessageOnSaveParamName = "Friend plugin message on save";
+        private static string friendPluginMessageOnLoadParamName = "Friend plugin message on load";
+
+        public SomeFriendPluginAdapter()
+        {
+            RegisterParameter(
+                friendPluginMessageOnSaveParamName,
+                "",
+                delegate (string strValue)
+                {
+                    return strValue;
+                });
+
+            RegisterParameter(
+                friendPluginMessageOnLoadParamName,
+                "",
+                delegate (string strValue)
+                {
+                    return strValue;
+                });
+        }
+
+        public override string Name()
+        {
+            return "SomeFriendPlugin adaptered";
+        }
+
+        public override string Description()
+        {
+            return "Shows message on save/load";
+        }
+
+        public override int Order()
+        {
+            return 3;
+        }
+
+        public override byte[] ProcessDataOnSave(string path, SerializationFormat serializationFormat, byte[] data)
+        {
+            friendPlugin.DoSave(
+                (string)ParametersInfo[friendPluginMessageOnSaveParamName].Value);
+
+            return data;
+        }
+
+        public override byte[] ProcessDataOnLoad(string path, SerializationFormat serializationFormat, byte[] data)
+        {
+            friendPlugin.DoLoad(
+                (string)ParametersInfo[friendPluginMessageOnLoadParamName].Value);
+
+            return data;
+        }
+
+    }
+}
